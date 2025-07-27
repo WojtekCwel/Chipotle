47b400620ff47119461e892f7f38e49039c21303
FranylFen <volti1337@onet.pl>
2025-07-27 22:09:36 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -92,7 +92,7 @@ const closePreview = document.querySelector('.close-preview');
 
 input.addEventListener('input', (event) => {
     const imgurUrl = event.target.value;
-    if (imgurUrl.includes('imgur.com')) {
+    if (imgurUrl.includes('ibb.co')) {
         // Clear previous image from localStorage
         localStorage.removeItem('userImage');
         input.setAttribute("selected");
@@ -190,7 +190,7 @@ document.querySelector(".go").addEventListener('click', () => {
     params.set("sex", sex)
     
     const imageInput = document.querySelector("#image");
-    if (!imageInput.value || !imageInput.value.includes('imgur.com')){
+    if (!imageInput.value || !imageInput.value.includes('ibb.co')){
         empty.push(imageInput.parentElement);
         imageInput.parentElement.classList.add("error_shown");
     } else {
